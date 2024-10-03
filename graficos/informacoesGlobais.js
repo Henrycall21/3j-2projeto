52426df7ae7d7780a50d738eb8c5c2fd88376b6a
Henrycall21 <160125220+Henrycall21@users.noreply.github.com>
2024-10-03 08:07:33 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,11 +2,18 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function visualizarDadosGlobais(){
     const res = await fetch(url);
+    const secao = document.querySelector('.graficos-container')
     const dados = await res.json();
+    const totalPessoasMundo = dados.total_pessoas_mundo
+    const totalPessoasConectadas = dados.total_pessoas_conectadas
+    const tempoMedio = dados.tempo_medio
     console.log(dados);
     const paragrafo = document.createElement('p');
+    //* cria um elemento <p> no HTML
     paragrafo.classList.add('graficos-texto')
-
+    //* adiciona a classe 'graficos-texto' no elemento <p>
+    paragrafo.innerHTML = `voce sabia que o mundo tem ${totalPessoasMundo} de habitantes e dentes ${totalPessoasConectadas} estao conectadas a alguma rede social e ficam em media ${tempoMedio} conectadas.`
+    secao.appendChild(paragrafo)
 }
 
 visualizarDadosGlobais();
